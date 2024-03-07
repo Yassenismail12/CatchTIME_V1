9de9c8fd789a2b6f2453e54cd974ddfef1b0a389
yasin mohamed <yasin.mohamed.AI@mau.edu.eg>
2024-03-07 12:24:45 +0200

    Update

--- Models/CatchTIMEContext.cs
+++ Models/CatchTIMEContext.cs
@@ -26,7 +26,7 @@ namespace TESTT.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-32VL26T\\SQLEXPRESS;Initial Catalog=CatchTime;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+                optionsBuilder.UseSqlServer("Data Source=YASSEN-PC\\TESTED;Initial Catalog=CatchTime;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
             }
             optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
         }
